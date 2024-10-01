cc31986abf1012446f02e36528a6aa10e332eb45
Edwin <Edwinallen94@aol.com>
2024-10-01 15:38:29 -0400

    Transfered files from codedsandbox to VScode

---
+++ script.js/script.mjs
@@ -0,0 +1,13 @@
+// Request data from an external API using fetch and Axios.
+
+// Create an interactive, dynamic webpage that serves content from an external API.
+
+// Use async/await and Promises to create synchronous logic in asynchronous actions.
+
+// Using fetch or Axios, POST data to and DELETE data from an external API.
+
+// Part 1: The API
+
+// Went over documnetation of the CAT API
+
+// Part 2: Tasks
